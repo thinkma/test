eb0f5dbe5b79130f848ce0b5e91d2ace7190241c
mawei <mawei@10.100.1.76>
2018-03-28 18:20:00 +0800

    错误提交

--- Test/Test/ViewController.m
+++ Test/Test/ViewController.m
@@ -18,8 +18,10 @@
     [super viewDidLoad];
     
     NSLog(@"版本为0.0.2");
-    
     NSLog(@"版本为0.0.2");
+    
+    
+    NSLog(@"版本为0.0.5");
 
     
     // Do any additional setup after loading the view, typically from a nib.
